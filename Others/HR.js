b353f7906311068d1577ce5d71a40e027acc4e34
Aditya V B <adiballurkar34@gmail.com>
2019-09-07 19:27:37 +0530

    all commits

---
+++ Others/HR.js
@@ -0,0 +1,10 @@
+function vowelsandConsonants(s) {
+    for (var i=0;i<s.length;i++){
+        if (s[i] == "a","e","i","o","u") {
+            console.log(s[i]);
+        }else {
+            console.log(s[i])
+        }
+    }
+}
+vowelsandConsonants("Javascript")
---
+++ Others/recursionp3.js
@@ -0,0 +1,5 @@
+
+function robot(n,a,b) {
+    if (n <= 0) return; robot(n-1, a, b+n); console.log(n,a,b); robot(n-1, b, a+n);
+    }
+console.log(robot(2,5,2))
