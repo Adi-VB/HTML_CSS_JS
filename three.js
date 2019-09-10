f1706e6eaccf2aad312632defd62621437bd2a73
Aditya V B <adiballurkar34@gmail.com>
2019-09-10 18:55:30 +0530

    "three numbers"

--- three.js
+++ three.js
@@ -1,112 +1,42 @@
-// function three(num) {
-//     var a, b, c, i;
-//     var num = [a,b,c];
-//     var readlineSync = require('readline-sync');
-//     var num = readlineSync.question("enter the numbers a,b,c: ")
-//     do {
-//         switch (num) {
-//             case (num.valueof(0) >= num.valueof(1) && num.valueof(1)>=num.value0f(2)):
-//                 console.log("A is the greatest no");          
-//                 break;
-//             case (num.valueof(0) <= num.valueof(1) && num.valueof(1)<=num.value0f(2)):
-//                 console.log("C is the greatest no");
-//                 break;
-//             case (num.valueof(1) >= num.valueof(0) && num.valueof(1)>=num.value0f(2)):
-//                 console.log("B is the Greatest no")
-//             default:
-//                 console.log("all the numbers entered are equal")
-//                 break;
-//         }
-//     } while (i<num.length);
-// }
-// three();
+var readlineSync = require('readline-sync');
 
-// function three() {
-//     var a, b, c;
-//     var num = [a, b, c];
-//     var readlineSync = require('readline-sync');
-//     var a = readlineSync.question("enter the 1st no: ");
-//     var b = readlineSync.question("enter the 2nd no: ");
-//     var c = readlineSync.question("enter the 3rd no: ");
-//     switch (num = [a,b,c]) {
-//         case (a >= b && b >= c):
-//             console.log("A is the greatest no");
-//             break;
-//         case (a <= b && b <= c):
-//             console.log("C is the greatest no");
-//             break;
-//         case (b >= a && b >= c):
-//             console.log("B is the Greatest no");
-//             break;
-//         default:
-//             console.log("all the numbers entered are equal")
-//             break;
-//     }
-// }
-// three();
-
-
-// function three(num) {
-//     var num = [a, b, c];
-//     var readlineSync = require('readline-sync');
-//     var a =readlineSync.question("enter the 1st no: ");
-//     var b =readlineSync.question("enter the 2nd no: ");
-//     var c =readlineSync.question("enter the 3rd no: ");
-//     switch (num) {
-//         case (num.valueOf(a) >= num.valueOf(b) && num.valueOf(b) >= num.valueOf(c)):
-//             console.log("A is the greatest no");
-//             break;
-//         case (num.valueOf(a) <= num.valueOf(b) && num.valueOf(b) <= num.valueOf(c)):
-//             console.log("C is the greatest no");
-//             break;
-//         case (num.valueOf(b) >= num.valueOf(a) && num.valueOf(b) >= num.valueOf(c)):
-//             console.log("B is the Greatest no")
-//             break;
-//         default:
-//             console.log("all the numbers entered are equal")
-//             break;
-//     }
-// }
-// three();
-
-function three(num) {
-    var num = [a, b, c];
-    var readlineSync = require('readline-sync');
-    var a =readlineSync.question("enter the 1st no: ");
-    var b =readlineSync.question("enter the 2nd no: ");
-    var c =readlineSync.question("enter the 3rd no: ");
-    switch (num) {
-        case (num[0] > num[1] && num[1] > num[2]):
-            console.log("A is the greatest no");
-            break;
-        case (num[0] < num[1] && num[1] < num[2]):
-            console.log("C is the greatest no");
-            break;
-        case (num[1] > num[0] && num[1] > num[2]):
-            console.log("B is the Greatest no")
-            break;
-        default:
-            console.log("all the numbers entered are equal")
-            break;
+function three() {
+    var a = Number(readlineSync.question("enter the 1st no: "));
+    var b = Number(readlineSync.question("enter the 2nd no: "));
+    var c = Number(readlineSync.question("enter the 3rd no: "));
+    if (a % 1 == 0 && b % 1 == 0 && c % 1 == 0) {
+        if (a > b && b > c) {
+            console.log("A");
+        } else if (a < b && b < c) {
+            console.log("C");
+        } else if (b > a && b > c) {
+            console.log("B");
+        } else if (a == b && a == c) {
+            console.log("The no's are equal");
+        } else {
+            console.log("input a valid number");
+            three();
+        }
+    } else {
+        console.log("Please enter valid number's");
+        three();
     }
+    three1();
 }
 three();
 
-// function three2(num) {
-//     var num = [a, b, c];
-//         var i, a, b, c;
-//         var readlineSync = require('readline-sync');
-//         var a =readlineSync.question("enter the 1st no: ");
-//         var b =readlineSync.question("enter the 2nd no: ");
-//         var c =readlineSync.question("enter the 3rd no: ");
-//         do {
-//             console.log("A is the Greatest")
-//         } while (a>=b && b>=c);
-//         do {
-//             console.log("C is the Greatest");
-//         } while (a<=b && b<=c);
-//         do {
-//             console.log("B is the Greatest")
-//         } while (b>=a && b>=c);
-// }
-// three2();
+function three1() {
+    var s = readlineSync.question("Do you whish to continue y/n : ");
+    switch (s) {
+        case (s = "y"):
+            three();
+            break;
+        case (s = "n"):
+            console.log("thank you !!!!");
+            break;
+        default:
+            console.log("Enter a Valid Responce:");
+            three1();
+            break;
+    }
+}
