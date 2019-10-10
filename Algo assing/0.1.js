81821aa32472580c16ae0f9707c4c923624a22d3
Aditya V B <adiballurkar34@gmail.com>
2019-10-10 15:11:04 +0530

    "updated"

--- Algo assing/0.1.js
+++ Algo assing/0.1.js
@@ -1,17 +1,36 @@
 var readlineSync = require('readline-sync');
 
-// var M = readlineSync.question("Enter the no of matrices: ")
+var M = readlineSync.question("Enter the no of matrices: ")
 var N = readlineSync.question("Enter the order of the NxN matrix: ");
 var arr = [];
 var arr1 = [];
+var arr2 = [];
+var result = [];
+var result1 = [];
 
-
-for (let i = 0; i < N; i++) {
-    arr = [];
-    for (let j = 0; j < N; j++) {
-        var n = Number(readlineSync.question("Enter the ele: "))
-        arr.push(n);
+for (let k = 0; k < M; k++) {
+    arr1 = [];
+    for (let i = 0; i < N; i++) {
+        arr = [];
+        for (let j = 0; j < N; j++) {
+            var n = Number(readlineSync.question("Enter the ele: "))
+            arr.push(n);
+        }
+        arr1.push(arr)
     }
-    arr1.push(arr)
+    console.log(arr1);
+    arr2.push(arr1);
 }
-console.log(arr1);
+console.log(arr2);
+
+const addMatrice = () => {
+
+    arr2.map((e, index) => {
+        result.push(
+            e.map((x, index2) => x + arr1[index][index2])
+        )
+    })
+    console.log('sum ->', result)
+}
+
+addMatrice()
--- Code_along/code_ along1.js
+++ Code_along/code_ along1.js
@@ -117,3 +117,54 @@
 // }
 
 //node fs module
+
+// Command line inputs and command line arguments
+// console.log(process.argv)
+//  require('yargs').argv
+
+
+// var inputs = [];
+// for (let i = 2; i < process.argv.length; i++) {
+//     inputs.push(process.argv[i]);
+// }
+
+// function sum(a,b) {
+//     console.log(a+b);
+// }
+// sum(Number(inputs[0]), Number(inputs[1]));
+
+// yargs:
+
+// const yargs = require('yargs');
+
+// const argv = yargs
+//     .command('lyr', 'Tells whether an year is leap year or not', {
+//         year: {
+//             description: 'the year to check for',
+//             alias: 'y',
+//             type: 'number',
+//         }
+//     })
+//     .option('time', {
+//         alias: 't',
+//         description: 'Tell the present Time',
+//         type: 'boolean',
+//     })
+//     .help()
+//     .alias('help', 'h')
+//     .argv;
+
+// if (argv.time) {
+//     console.log('The current time is: ', new Date().toLocaleTimeString());
+// }
+
+// if (argv._.includes('lyr')) {
+//     const year = argv.year || new Date().getFullYear();
+//     if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+//         console.log(`${year} is a Leap Year`);
+//     } else {
+//         console.log(`${year} is NOT a Leap Year`);
+//     }
+// }
+
+// console.log(argv);
